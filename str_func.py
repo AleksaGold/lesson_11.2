54bd6aa15a49f6eab42a220746063d6167eb1fd0
AleksaGold <167828288+AleksaGold@users.noreply.github.com>
2024-04-26 13:45:09 -0400

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,15 +1,15 @@
-def upper_letters(word):
+def capitalize_letters(word):
     """
     Функция принимает на вход строку и
-    возвращает ее со всеми заглавными буквами
-    """
-    return word.upper()
-
-
-def capital_letters(word):
-    """
-    Принимает на вход строку и
     возвращает заглавными первые буквы каждого слова в строке
     """
     return word.capitalize()
+
+
+def upper_letters(word):
+    """
+    Принимает на вход строку и
+    возвращает ее со всеми заглавными буквами
+    """
+    return word.upper()
 
